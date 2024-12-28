69cce93d7fdc0083f96e564560ba6aa6bf12d1da
tripgraff <tripgraffmac@Noutbuk-tripgraff.local>
2024-12-29 00:47:36 +0300

    небольшшие исправления и доработки

--- js/animations.js
+++ js/animations.js
@@ -60,3 +60,21 @@ function updateScroll() {
 }
 
 window.addEventListener('scroll', updateScroll);
+
+function addSmoothScroll(link) {
+    link.addEventListener('click', onLinkClick);
+}
+
+function onLinkClick(event) {
+    event.preventDefault();
+
+    document.querySelector(event.target.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+    })
+};
+
+document.querySelectorAll('a[href^="#"]').forEach(aHreaf => {
+    addSmoothScroll(aHreaf);
+})
+
+addSmoothScroll(document.querySelector('.more-button'));
